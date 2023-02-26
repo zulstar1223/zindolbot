bfb16e33a69ddeee9661cbb20309095d08b64575
zulstar1223 <100339561+zulstar1223@users.noreply.github.com>
2023-02-26 23:05:41 +0900

    Update botrun.py

--- botrun.py
+++ botrun.py
@@ -34,4 +34,4 @@ async def 길들이기(ctx,*,text):
 @bot.command()
 async def 길들이기포인트(ctx, *, text):
     await ctx.send(tamingpoint.count(text))
-bot.run('MTA3OTIxMzI3MzE4ODEzOTA1OQ.Ga6s_-.W7MrS58VXuZhPGJcbJ56h4HphOvjG_ofmSPgMU')
+bot.run('MTA3OTIxMzI3MzE4ODEzOTA1OQ.GGPaIZ.uhWOY5HRznv32DM44ZFID8HQ3yTc0NoqEsnU3Y')
